398460c1eafa10de51ed2897823e50ee8510b894
PsProfi <you@example.com>
2024-10-09 08:49:07 +0300

    photo enabled

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFFFFF8F0),
-        body: Column(
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Row(
-                children: [Icon(Icons.call), Text("+380000000000")],
+            Expanded(
+              child: Column(
+                children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [Icon(Icons.call), Text("+380637116280")],
+                        ),
+                        Row(children: [
+                          Icon(Icons.alternate_email),
+                          Text("pavlyk.smal@gmail.com")
+                        ])
+                      ]),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Text("Hi, I'm Pavlo",
+                      style: TextStyle(fontSize: 16, color: Color(0xFF291854))),
+                  Text("Life-enjoyer",
+                      style: TextStyle(
+                          fontSize: 48,
+                          color: Color(0xFF190D37),
+                          fontWeight: FontWeight.bold))
+                ],
               ),
-              Row(children: [
-                Icon(Icons.alternate_email),
-                Text("olegnovosad@gmail.com")
-              ])
-            ]),
-            SizedBox(height: 48,),
-            Text("Hi, I'm Oleg Novosad",
-                style: TextStyle(fontSize: 16, color: Color(0xFF291854))),
-            Text("Application Architect and Music Producer",
-                style: TextStyle(
-                    fontSize: 48,
-                    color: Color(0xFF190D37),
-                    fontWeight: FontWeight.bold))
+            ),
+            Expanded(
+                child: ClipRRect(
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(200)),
+                    child: Image.asset("assets/haker-1024x645.jpg.webp",
+                        height: 600, fit: BoxFit.cover)))
           ],
         ),
       ),
